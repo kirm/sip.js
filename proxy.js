1ce429a0b0a42bdfbb1c16bb3dab5b86e11e93c6
Kirill Mikhailov <kirill.mikhailov@gmail.com>
2011-02-18 12:18:25 +0300

    Rewritten proxy module

--- proxy.js
+++ proxy.js
@@ -1,135 +1,80 @@
-// simple outbound proxy server
-var sys = require('sys');
-var sip = require('./sip.js');
-var crypto = require('crypto');
+var sip=require('sip');
+var util=require('sys');
 
-function branch(rq) {
-  var md5 = crypto.createHash('md5');
+var contexts = {};
 
-  md5.update(rq.headers.via[0].params.branch);
-  if(rq.headers.from.params.tag)
-    md5.update(rq.headers.from.params.tag);
+function onRequest(rq, route) {
+  var ctx = {};
+  var branch = rq.headers.via[0].params.branch;
+  contexts[branch] = ctx;
 
-  if(rq.headers.to.params.tag)
-    md5.update(rq.headers.to.params.tag);
+  route({
+    send: function(msg, callback) {
+      if(msg.method) {
+        var uri = msg.uri;
 
-  md5.update(rq.headers['call-id']);
-  md5.update(rq.headers.cseq.seq);
-  md5.update(rq.uri);
+        sip.send(msg, function(rs, remote) {
+          if(rs.status >= 100 && rs.status <= 199) {
+            var cancel = {
+              method: 'CANCEL',
+              uri: uri,
+              headers: {
+                via: [rs.headers.via[0]],
+                cseq: {method: 'CANCEL', seq: rs.headers.cseq.seq},
+                'call-id': rs.headers['call-id'],
+                from: rs.headers.from,
+                to: rs.headers.to,
+                'content-length': 0
+              }
+            }
 
-  return 'z9hG4bK'+md5.digest('hex');  
-}
+            ctx.cancel = function() { 
+              ctx.cancelled = false;
+              sip.send(cancel);
+            }
 
-function proxyRequest(transport, rq, target) {
-  rq.uri = target;
+            if(ctx.cancelled)
+              ctx.cancel();
+          }
+          else
+            ctx.cancel = null;
 
-  sip.resolve(target, function(address) {
-    if(address.length === 0) {
-      transport(sip.makeResponse(rq, 503), {protocol: rq.headers.via[0].protocol, address: rq.headers.via[0].received, port: rq.headers.via[0].port});
+          callback && callback(rs, remote);
+        });
+      }
+      else {
+       if(msg.headers.via[0].params.branch !== branch)
+          msg.headers.via.shift()
+//        assert(msg.headers.via[0].params.branch === branch);
+
+        if(msg.status >= 200)
+          delete contexts[branch];
+
+        sip.send(msg);
+      }
     }
+  },
+  rq);
+};
 
-    rq.headers.via.unshift({params: {branch: branch(rq)}});
-    transport(rq, address[0]);
+exports.start = function(options, route) {
+  sip.start(options, function(rq) {
+    if(rq.method === 'CANCEL') {
+      var ctx = contexts[rq.headers.via[0].params.branch];
+
+      if(ctx) {
+        sip.send(sip.makeResponse(rq, 200));
+       
+        ctx.cancelled = true; 
+        ctx.cancel && ctx.cancel();
+      }
+      else {
+        sip.send(sip.makeResponse(rq, 481));
+      }
+    }
+    else {
+      onRequest(rq, route);
+    }
   });
-}
-
-function proxyResponse(transport, rs) {
-  rs.headers.via.shift();
-
-  transport(rs, {protocol: rs.headers.via[0].protocol, address: rs.headers.via[0].params.received, port: rs.headers.via[0].port}); 
-}
-
-function proxyCore(transport, router, message, remote) {
-  var trn = transaction.get(message);
-
-  if(trn) {
-    trn.message(message);
-    return;
-  }
-
-  try {
-    if(message.method) {
-      message.headers.via[0].params.received=remote.address;
-      router.call({
-        forward: function(target) {
-          return proxyRequest(transport, message, target); 
-        },
-        transactForward: function(target) {
-          if(message.method === 'ACK')
-            return proxyRequest(transport, message, target);
-
-          var server = transaction.createServerTransaction(message, remote);
-          var client = transaction.createClientTransaction(
-            {
-              method:message.method,
-              uri: target,
-              headers: message.headers,
-              content: message.content
-            },
-            function(rs, remote) { 
-              rs.headers.via.shift();
-              server.send(rs);
-            }); 
-        },
-        respond: function(rs) {
-          if(typeof rs === 'number')
-            rs = sip.makeRespose(message, rs);
-
-          transport(rs, {protocol: rs.headers.via[0].protocol, address: remote.address, port: rq.headers.via[0].port});
-        },
-        transactResponse: function(rs) {
-          if(typeof rs === 'number')
-            rs = sip.makeResponse(message, rs);
-         
-          transaction.createServerTransaction(message, remote).send(rs);
-        }
-      }, message, remote);
-    }
-    else
-      proxyResponse(transport, message);
-  } 
-  catch(e) {
-    sys.debug(e + e.stack);
-  }
-}
-
-function route(rq, remote) {
-  if(remote.address == '172.16.1.10' && remote.port == 5060) {
-    this.transactForward(sip.parseUri(rq.uri));
-  }
-  else if(rq.method === 'REGISTER') {
-    this.respond(403);
-  }
-  else {
-    var uri = sip.parseUri(rq.uri);
-    uri.host = '172.16.1.10';
-    uri.port = 5060;
-    uri.params.transport = remote.protocol.toLowerCase();
-    this.transactForward(uri);
-  }
-}
-
-function send(message, remote) {
-  var cn = transport.open(remote);
-  
-  try {
-    cn.send(message);
-  }
-  finally {
-    cn.release();
-  }
-}
-
-var transport = sip.makeTransport({
-  logger: {
-    send: function(m, remote) { sys.log(JSON.stringify(remote) + '--->>' + JSON.stringify(m)); },
-    recv: function(m, remote) { sys.log(JSON.stringify(remote) + '<<---' + JSON.stringify(m)); }
-  }
-},
-function(message, remote) {
-  //sys.debug(JSON.stringify(['recv', remote, message]));
-  proxyCore(send, route, message, remote);
-});
-
-var transaction = sip.makeTransactionLayer({}, transport.open.bind(transport));
+};
 
