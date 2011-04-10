5003093f1db811d60df92371f90e4c4aa538b9da
Kirill Mikhailov <kirill.mikhailov@gmail.com>
2011-04-10 12:08:42 +0400

    Cleaned up proxy module.
    This changes its API a bit, instead of passing context as a parameter,
    we bind it through top via header. So there is no longer cx parameter in
    onRequest callback. Use proxy.send instead cx.send.

--- proxy.js
+++ proxy.js
@@ -3,76 +3,89 @@ var util=require('sys');
 
 var contexts = {};
 
-function onRequest(rq, route) {
-  var ctx = {};
-  var branch = rq.headers.via[0].params.branch;
-  contexts[branch] = ctx;
+function makeContextId(msg) {
+  var via = msg.headers.via[0];
+  return [via.params.branch, via.protocol, via.host, via.port, msg.headers['call-id'], msg.headers.cseq.seq];
+}
 
-  function forwardResponse(msg) {
-    if(msg.headers.via[0].params.branch !== branch)
-      msg.headers.via.shift()
+function defaultCallback(rs) {
+  rs.headers.via.shift();
+  exports.send(rs);
+}
 
-    if(msg.status >= 200)
-      delete contexts[branch];
 
-    sip.send(msg);
+exports.send = function(msg, callback) {
+  var ctx = contexts[makeContextId(msg)];
+
+  if(!ctx) {
+    sip.send.apply(arguments);
+    return;
+  }
+ 
+  return msg.method ? forwardRequest(ctx, msg, callback || defaultCallback) : forwardResponse(ctx, msg);
+};
+
+
+function forwardResponse(ctx, rs, callback) {
+  if(+rs.status >= 200) {
+    delete contexts[makeContextId(rs)];
   }
 
-  route({
-    send: function(msg, callback) {
-      if(msg.method) {
-        var uri = msg.uri;
+  sip.send(rs);
+}
 
-        sip.send(msg, function(rs, remote) {
-          if(rs.status >= 100 && rs.status <= 199) {
-            var cancel = {
-              method: 'CANCEL',
-              uri: uri,
-              headers: {
-                via: [rs.headers.via[0]],
-                cseq: {method: 'CANCEL', seq: rs.headers.cseq.seq},
-                'call-id': rs.headers['call-id'],
-                from: rs.headers.from,
-                to: rs.headers.to,
-                'content-length': 0
-              }
-            }
 
-            ctx.cancel = function() { 
-              ctx.cancelled = false;
-              sip.send(cancel);
-            }
-
-            if(ctx.cancelled)
-              ctx.cancel();
-          }
-          else
-            ctx.cancel = null;
-
-          if(callback) 
-            callback(rs, remote);
-          else 
-            forwardResponse(rs);
-        });
-      }
-      else {
-        forwardResponse(msg);
-      }
+function sendCancel(rq, via) {
+  sip.send({
+    method: 'CANCEL',
+    uri: rq.uri,
+    headers: {
+      via: [via],
+      to: rq.headers.to,
+      from: rq.headers.from,
+      'call-id': rq.headers['call-id'],
+      cseq: {method: 'CANCEL', seq: rq.headers.cseq.seq}
     }
-  },
-  rq);
+  });
+}
+
+
+function forwardRequest(ctx, rq, callback) {
+  sip.send(rq, function(rs, remote) {
+    if(+rs.status < 200) {
+      var via = rs.headers.via[0];
+      ctx.cancellers[rs.headers.via[0].params.branch] = function() { sendCancel(rq, via); };
+
+      if(ctx.cancelled)
+        sendCancel(rq, via);
+    }
+    else {
+      delete ctx.cancellers[rs.headers.via[0].params.branch];
+    }
+
+    callback(rs, remote);
+  });
+}
+
+
+function onRequest(rq, route, remote) {
+  contexts[makeContextId(rq)] = { cancellers: {} };
+  route(sip.copyMessage(rq), remote);
 };
+
 
 exports.start = function(options, route) {
   sip.start(options, function(rq) {
     if(rq.method === 'CANCEL') {
-      var ctx = contexts[rq.headers.via[0].params.branch];
+      var ctx = contexts[makeContextId(rq)];
 
       if(ctx) {
         sip.send(sip.makeResponse(rq, 200));
        
-        ctx.cancelled = true; 
-        ctx.cancel && ctx.cancel();
+        ctx.cancelled = true;
+        if(ctx.cancellers) {
+          Object.keys(ctx.cancellers).forEach(function(c) { ctx.cancellers[c](); });
+        }
       }
       else {
         sip.send(sip.makeResponse(rq, 481));
@@ -83,4 +96,6 @@ exports.start = function(options, route) {
     }
   });
 };
+
+exports.stop = sip.stop;
 
