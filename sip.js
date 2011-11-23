32cc024b37f7cec8613a6894445bc116fc77555f
Gustavo <gustavogb@gmail.com>
2011-11-24 00:06:34 +0100

    Use Route header to route requests if available

--- sip.js
+++ sip.js
@@ -1039,7 +1039,9 @@ function makeTransactionLayer(options, transport) {
 
       var transaction = rq.method === 'INVITE' ? createInviteClientTransaction : createClientTransaction;
 
-      resolve(parseUri(rq.uri), function(address) {
+      var hop = rq.headers.route || rq.uri;
+
+      resolve(parseUri(hop), function(address) {
         var onresponse;
 
         function next() {
