1c623ba8cc4e53eabe30e0e2b48f1d3689045005
Kirill Mikhailov <kirill.mikhailov@gmail.com>
2013-05-25 09:04:01 +0400

    Fixed incorrect concatenation of resolve4 and resolve6 results in resolve46. Should fix part 2 of issue #41
    Signed-off-by: Kirill Mikhailov <kirill.mikhailov@gmail.com>

--- sip.js
+++ sip.js
@@ -665,7 +665,7 @@ function resolve(uri, action) {
     resolve4(host, function(e4, a4) {
       resolve6(host, function(e6, a6) {
         if((a4 || a6) && (a4 || a6).length)
-          cb(null, a4.concat(a6));
+          cb(null, (a4 || []).concat(a6 || []));
         else
           cb(e4 || e6, []);
       });
@@ -703,6 +703,7 @@ function resolve(uri, action) {
           });
         }
         else if(0 === n) {
+          console.log(e);
           if(addresses.length) {
             action(addresses);
           }
