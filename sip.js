127db580afe409d067a85ac4224a1d934ff0e861
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-06-18 20:10:11 +0000

    parseParams: param without value set to null instead of undefined for consitency with parseUri and because JSON.stringify discard undefined

--- sip.js
+++ sip.js
@@ -71,7 +71,7 @@ function parseParams(data, hdr) {
   var re = /\s*;\s*([\w\-.!%*_+`'~]+)(?:\s*=\s*([\w\-.!%*_+`'~]+|"[^"\\]*(\\.[^"\\]*)*"))?/g; 
   
   for(var r = applyRegex(re, data); r; r = applyRegex(re, data)) {
-    hdr.params[r[1].toLowerCase()] = r[2];
+    hdr.params[r[1].toLowerCase()] = r[2] || null;
   }
 
   return hdr;
