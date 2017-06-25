fc47cfc098aea40d0f61353267e2be4e441340d1
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2017-06-25 18:47:16 +0000

    corectly parse AoR like To: "bob" <sip:bob@example.com;foo="<bar>">

--- sip.js
+++ sip.js
@@ -93,7 +93,7 @@ function parseGenericHeader(d, h) {
 }
 
 function parseAOR(data) {
-  var r = applyRegex(/((?:[\w\-.!%*_+`'~]+)(?:\s+[\w\-.!%*_+`'~]+)*|"[^"\\]*(?:\\.[^"\\]*)*")?\s*\<\s*([^>]*)\s*\>|((?:[^\s@"<]@)?[^\s;]+)/g, data);
+  var r = applyRegex(/((?:[\w\-.!%*_+`'~]+)(?:\s+[\w\-.!%*_+`'~]+)*|"[^"\\]*(?:\\.[^"\\]*)*")?\s*\<\s*((?:[^">]|(?:"[^"]*"))*)\s*\>|((?:[^\s@"<]@)?[^\s;]+)/g, data);
 
   return parseParams(data, {name: r[1], uri: r[2] || r[3] || ''});
 }
